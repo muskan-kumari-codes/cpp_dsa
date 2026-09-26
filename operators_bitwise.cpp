b08c60947831f2770020742c1ef3cf7915fe37a3
muskan-kumari-codes <muskanmalikyadav01@gmail.com>
2026-09-26 19:50:19 +0530

    bitwise operations

---
+++ operators_bitwise.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"enter the value of a "<<endl;
+    cin>>a;
+    int b;
+    cout<<"enter the value of b "<<endl;
+    cin>>b;
+    cout<<"a&b is "<<(a&b)<<endl;
+    cout<<"a|b is "<<(a|b)<<endl;
+    cout<<"~a is "<< ~a <<endl;
+    cout<<"a^b is "<<(a^b)<<endl;
+}
